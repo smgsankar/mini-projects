61bebd78cff36ea26e71c080e017ab7b21db7b65
smgsankar <gomzsankar@gmail.com>
2024-07-27 00:08:24 +0530

    fix: add hover/active effect for day cell

--- todo/src/Calendar/components/MonthDayCell.jsx
+++ todo/src/Calendar/components/MonthDayCell.jsx
@@ -3,9 +3,12 @@ import PropTypes from "prop-types";
 export function MonthDayCell({ day, isCurrentMonth, isToday }) {
   return (
     <div
+      tabIndex={1}
       className={`flex items-center justify-center rounded-xl p-2 ${
         isCurrentMonth ? "text-accent-white" : "text-accent-grey"
-      } ${isToday ? "bg-accent-green" : ""}`}
+      } ${
+        isToday ? "bg-accent-green" : ""
+      } hover:outline hover:outline-accent-green active:outline active:outline-accent-green`}
     >
       {day.getDate()}
     </div>
