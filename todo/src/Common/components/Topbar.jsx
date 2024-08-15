6d715c7ab2294ad0f1f268feeb98903ea7b6c7f6
smgsankar <gomzsankar@gmail.com>
2024-08-15 22:51:48 +0530

    fix: change title font to cursive

--- todo/src/Common/components/Topbar.jsx
+++ todo/src/Common/components/Topbar.jsx
@@ -7,7 +7,7 @@ export function Topbar() {
   return (
     <div
       onClick={handleClick}
-      className="fixed top-0 left-0 bg-white w-full font-semibold text-2xl text-center py-5 shadow-bottomElevated z-10"
+      className="fixed top-0 left-0 bg-white w-full font-semibold text-2xl text-center py-5 shadow-bottomElevated z-10 font-[cursive]"
     >
       Pages AIO
     </div>
