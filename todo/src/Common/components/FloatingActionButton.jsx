859ce82ade1ea3d76b9996ad059e747964f3c5d7
smgsankar <gomzsankar@gmail.com>
2024-08-30 02:11:20 +0530

    feat: add FAB

---
+++ todo/src/Common/components/FloatingActionButton.jsx
@@ -0,0 +1,12 @@
+import { Add } from "../icons/Add";
+
+export function FloatingActionButton({ onClick }) {
+  return (
+    <button
+      className="absolute bottom-6 right-6 p-3 rounded-full bg-amber-800 text-white shadow-lg"
+      onClick={onClick}
+    >
+      <Add size={40} />
+    </button>
+  );
+}
